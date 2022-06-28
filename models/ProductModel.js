c23190278972d99ddc8a8f85de3c15f062801c30
ibz <ibrahim_kahil@dlsu.edu.ph>
2022-06-28 14:09:08 +0800

    Recopied files

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -8,16 +8,8 @@ var ProductSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    category: {
-        type: String,
-        required: true
-    },
     price: {
         type: Number,
-        required: true
-    },
-    image: {
-        type: String,
         required: true
     }
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-        let slideIndex = 1;
-        showSlides(slideIndex);
+    let slideIndex = 1;
+    showSlides(slideIndex);
 
-        function plusSlides(n){
-            showSlides(slideIndex += n);
-        }
+    function plusSlides(n){
+        showSlides(slideIndex += n);
+    }
 
-        function showSlides(n){
-            let i;
-            let slides = document.getElementsByClassName("slide");
-            
-            if (n > slides.length){slideIndex = 1}
+    function showSlides(n){
+        let i;
+        let slides = document.getElementsByClassName("slide");
+        
+        if (n > slides.length){slideIndex = 1}
 
-            if (n < 1){slideIndex = slides.length}
+        if (n < 1){slideIndex = slides.length}
 
-             for (i=0; i < slides.length; i++){
-                 slides[i].style.display="none";
-             }
+         for (i=0; i < slides.length; i++){
+             slides[i].style.display="none";
+         }
 
-            slides[slideIndex-1].style.display = "block";
-        }
+        slides[slideIndex-1].style.display = "block";
+    }
 
-        $(".prev").on("click", function(){plusSlides(-1)});
-        $(".next").on("click", function(){plusSlides(1)});
+    $(".prev").on("click", function(){plusSlides(-1)});
+    $(".next").on("click", function(){plusSlides(1)});
 })
