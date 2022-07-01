db2f66e1e46a465971e3f6892090957682f7afe8
Hannah Fong <hannah_regine_fong@dlsu.edu.ph>
2022-07-02 02:13:13 +0800

    Bag in progress

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-
-    $(".delete").click(function(deleteItem){
-        $(".order-container").each(function(){
-            
-            if ($(this).hasClass($(deleteItem.target).attr("class").substr(7, 7)))
-                $(this).css("display", "none");
-        })
-
-        console.log($(deleteItem.target).hasClass("1"));
-        console.log(this);
-    })
-
     const toggleButton = document.getElementById("toggle-button");
     const naviList = document.getElementById("right-text-container");
     
