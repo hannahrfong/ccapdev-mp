b8bd4cbdef513b2ce678966212fbd062abf429a1
Alessandra Gomez <94123165+alessgomez@users.noreply.github.com>
2022-07-02 22:40:40 +0800

    Fixed password format bug

--- public/js/sign-in.js
+++ public/js/sign-in.js
@@ -4,16 +4,24 @@ $(document).ready(function(){
     var submit = document.getElementById("signin-submit");
 
     email.onkeyup = function() {
-        if (email.value.length != 0 && pw.value.length != 0)
+        if (email.value.length != 0 && validatePW(pw.value))
             submit.disabled = false;
         else
             submit.disabled = true;
     }
 
     pw.onkeyup = function () {
-        if (email.value.length != 0 && pw.value.length != 0)
+        if (email.value.length != 0 && validatePW(pw.value))
             submit.disabled = false;
         else
             submit.disabled = true;
+    }
+
+    function validatePW (pw) {
+        var uppercaseLetters = /[A-Z]/g;
+        var specialChars = /\W|_/g;
+        var numbers = /[0-9]/g;
+        
+        return pw.match(uppercaseLetters) && pw.match(specialChars) && pw.match(numbers) && pw.length >= 8;
     }
 });
